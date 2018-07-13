bfcba5b724746372f932cf0e2a4d59dae210dc48
VinnyAmbesek <vinnyrpgmeister@gmail.com>
2018-07-13 14:21:16 -0300

    Ficha Herois v0.1

---
+++ Plugins/Sheets/Ficha Herois d20/output/rdkObjs/FichaHerois/05.Poderes.Poder.lfm.lua
@@ -0,0 +1,200 @@
+require("rrpg.lua");
+local __o_rrpgObjs = require("rrpgObjs.lua");
+require("rrpgGUI.lua");
+require("rrpgDialogs.lua");
+require("rrpgLFM.lua");
+require("ndb.lua");
+
+function newfrmPoder()
+    __o_rrpgObjs.beginObjectsLoading();
+
+    local obj = gui.fromHandle(_obj_newObject("form"));
+    local self = obj;
+    local sheet = nil;
+
+    rawset(obj, "_oldSetNodeObjectFunction", rawget(obj, "setNodeObject"));
+
+    function obj:setNodeObject(nodeObject)
+        sheet = nodeObject;
+        self.sheet = nodeObject;
+        self:_oldSetNodeObjectFunction(nodeObject);
+    end;
+
+    function obj:setNodeDatabase(nodeObject)
+        self:setNodeObject(nodeObject);
+    end;
+
+    _gui_assignInitialParentForForm(obj.handle);
+    obj:beginUpdate();
+    obj:setName("frmPoder");
+    obj:setHeight(25);
+    obj:setWidth(350);
+    obj:setMargins({top=2});
+
+			
+		local function askForDelete()
+			dialogs.confirmYesNo("Deseja realmente apagar essa habilidade?",
+								 function (confirmado)
+									if confirmado then
+										ndb.deleteNode(sheet);
+									end;
+								 end);
+		end;
+
+		local function showHabilidadePopup()
+			local pop = self:findControlByName("popPoder");
+				
+			if pop ~= nil then
+				pop:setNodeObject(self.sheet);
+				pop:showPopupEx("right", self);
+			else
+				showMessage("Ops, bug.. nao encontrei o popup para exibir");
+			end;				
+		end;
+	
+
+
+    obj.rectangle1 = gui.fromHandle(_obj_newObject("rectangle"));
+    obj.rectangle1:setParent(obj);
+    obj.rectangle1:setAlign("client");
+    obj.rectangle1:setColor("#202020");
+    obj.rectangle1:setHitTest(false);
+    obj.rectangle1:setName("rectangle1");
+
+    obj.edit1 = gui.fromHandle(_obj_newObject("edit"));
+    obj.edit1:setParent(obj);
+    obj.edit1:setVertTextAlign("center");
+    obj.edit1:setLeft(0);
+    obj.edit1:setTop(0);
+    obj.edit1:setWidth(200);
+    obj.edit1:setHeight(23);
+    obj.edit1:setField("nome");
+    obj.edit1:setName("edit1");
+
+    obj.edit2 = gui.fromHandle(_obj_newObject("edit"));
+    obj.edit2:setParent(obj);
+    obj.edit2:setVertTextAlign("center");
+    obj.edit2:setLeft(200);
+    obj.edit2:setTop(0);
+    obj.edit2:setWidth(50);
+    obj.edit2:setHeight(23);
+    obj.edit2:setField("custo");
+    obj.edit2:setHorzTextAlign("center");
+    obj.edit2:setName("edit2");
+
+    obj.edit3 = gui.fromHandle(_obj_newObject("edit"));
+    obj.edit3:setParent(obj);
+    obj.edit3:setVertTextAlign("center");
+    obj.edit3:setLeft(250);
+    obj.edit3:setTop(0);
+    obj.edit3:setWidth(25);
+    obj.edit3:setHeight(23);
+    obj.edit3:setField("usoAtual");
+    obj.edit3:setHorzTextAlign("center");
+    obj.edit3:setName("edit3");
+
+    obj.edit4 = gui.fromHandle(_obj_newObject("edit"));
+    obj.edit4:setParent(obj);
+    obj.edit4:setVertTextAlign("center");
+    obj.edit4:setLeft(275);
+    obj.edit4:setTop(0);
+    obj.edit4:setWidth(25);
+    obj.edit4:setHeight(23);
+    obj.edit4:setField("usoMax");
+    obj.edit4:setHorzTextAlign("center");
+    obj.edit4:setName("edit4");
+
+    obj.button1 = gui.fromHandle(_obj_newObject("button"));
+    obj.button1:setParent(obj);
+    obj.button1:setLeft(300);
+    obj.button1:setTop(1);
+    obj.button1:setWidth(23);
+    obj.button1:setHeight(23);
+    obj.button1:setText("i");
+    obj.button1:setName("button1");
+
+    obj.button2 = gui.fromHandle(_obj_newObject("button"));
+    obj.button2:setParent(obj);
+    obj.button2:setLeft(325);
+    obj.button2:setTop(1);
+    obj.button2:setWidth(23);
+    obj.button2:setHeight(23);
+    obj.button2:setText("X");
+    obj.button2:setName("button2");
+
+    obj.dataLink1 = gui.fromHandle(_obj_newObject("dataLink"));
+    obj.dataLink1:setParent(obj);
+    obj.dataLink1:setField("custo");
+    obj.dataLink1:setName("dataLink1");
+
+    obj._e_event0 = obj.button1:addEventListener("onClick",
+        function (self)
+            showHabilidadePopup();
+        end, obj);
+
+    obj._e_event1 = obj.button2:addEventListener("onClick",
+        function (self)
+            askForDelete();
+        end, obj);
+
+    obj._e_event2 = obj.dataLink1:addEventListener("onChange",
+        function (self, field, oldValue, newValue)
+            if sheet==nil then return end;
+            
+            			local node = ndb.getRoot(sheet);
+            			
+            			local custo = 0;
+            			local objetos = ndb.getChildNodes(node.campoDosPoderes);
+            			for i=1, #objetos, 1 do 
+            				custo = custo + (tonumber(objetos[i].custo) or 0);
+            			end;
+            			node.sumPoderes = custo;
+        end, obj);
+
+    function obj:_releaseEvents()
+        __o_rrpgObjs.removeEventListenerById(self._e_event2);
+        __o_rrpgObjs.removeEventListenerById(self._e_event1);
+        __o_rrpgObjs.removeEventListenerById(self._e_event0);
+    end;
+
+    obj._oldLFMDestroy = obj.destroy;
+
+    function obj:destroy() 
+        self:_releaseEvents();
+
+        if (self.handle ~= 0) and (self.setNodeDatabase ~= nil) then
+          self:setNodeDatabase(nil);
+        end;
+
+        if self.edit3 ~= nil then self.edit3:destroy(); self.edit3 = nil; end;
+        if self.edit2 ~= nil then self.edit2:destroy(); self.edit2 = nil; end;
+        if self.button1 ~= nil then self.button1:destroy(); self.button1 = nil; end;
+        if self.edit4 ~= nil then self.edit4:destroy(); self.edit4 = nil; end;
+        if self.rectangle1 ~= nil then self.rectangle1:destroy(); self.rectangle1 = nil; end;
+        if self.edit1 ~= nil then self.edit1:destroy(); self.edit1 = nil; end;
+        if self.button2 ~= nil then self.button2:destroy(); self.button2 = nil; end;
+        if self.dataLink1 ~= nil then self.dataLink1:destroy(); self.dataLink1 = nil; end;
+        self:_oldLFMDestroy();
+    end;
+
+    obj:endUpdate();
+
+     __o_rrpgObjs.endObjectsLoading();
+
+    return obj;
+end;
+
+local _frmPoder = {
+    newEditor = newfrmPoder, 
+    new = newfrmPoder, 
+    name = "frmPoder", 
+    dataType = "", 
+    formType = "undefined", 
+    formComponentName = "form", 
+    title = "", 
+    description=""};
+
+frmPoder = _frmPoder;
+rrpg.registrarForm(_frmPoder);
+
+return _frmPoder;
