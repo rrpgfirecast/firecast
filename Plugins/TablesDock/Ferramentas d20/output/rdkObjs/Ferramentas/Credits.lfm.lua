5c2ef884cb4a46c2e62d03e648a4544ce1cd28ea
VinnyAmbesek <vinnyrpgmeister@gmail.com>
2019-03-01 20:30:34 -0300

    Ferramentas RPGmeister 0.1.1 - Adicionada aba de creditos e plugin adicionado ao autoupdater.

--- Plugins/TablesDock/Ferramentas d20/output/rdkObjs/Ferramentas/Credits.lfm.lua
+++ Plugins/TablesDock/Ferramentas d20/output/rdkObjs/Ferramentas/Credits.lfm.lua
@@ -116,25 +116,25 @@ local function constructNew_frmTemplateCreditos()
 
     obj._e_event0 = obj.button1:addEventListener("onClick",
         function (_)
-            GUI.openInBrowser('https://github.com/rrpgfirecast/firecast/blob/master/Plugins/TablesDock/README.md')
+            GUI.openInBrowser('https://github.com/rrpgfirecast/firecast/blob/master/Plugins/TablesDock/Ferramentas%20d20/README.md')
         end, obj);
 
     obj._e_event1 = obj.button1:addEventListener("onClick",
         function (_)
             local install = Firecast.Plugins.installPlugin(rawget(updaterSheet,"stream"), true);
                             if install==false then
-                                GUI.openInBrowser('https://github.com/rrpgfirecast/firecast/blob/master/Plugins/TablesDock/AutoUpdater/output/AutoUpdater.rpk?raw=true');
+                                GUI.openInBrowser('https://github.com/rrpgfirecast/firecast/blob/master/Plugins/TablesDock/Ferramentas%20d20/output/Ferramentas%20d20.rpk?raw=true');
                             end;
         end, obj);
 
     obj._e_event2 = obj.button2:addEventListener("onClick",
         function (_)
-            GUI.openInBrowser('https://github.com/rrpgfirecast/firecast/blob/master/Plugins/TablesDock/AutoUpdater/output/AutoUpdater.rpk?raw=true')
+            GUI.openInBrowser('https://github.com/rrpgfirecast/firecast/blob/master/Plugins/TablesDock/Ferramentas%20d20/output/Ferramentas%20d20.rpk?raw=true')
         end, obj);
 
     obj._e_event3 = obj.button3:addEventListener("onClick",
         function (_)
-            GUI.openInBrowser('http://firecast.rrpg.com.br:90/a?a=pagRWEMesaInfo.actInfoMesa&mesaid=64070');
+            GUI.openInBrowser('https://my.firecastrpg.com/a?a=pagRWEMesaInfo.actInfoMesa&mesaid=64070');
         end, obj);
 
     function obj:_releaseEvents()
