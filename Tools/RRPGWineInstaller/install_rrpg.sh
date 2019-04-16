1bb0e5e4656d12b1567abf7a21384afb30683755
AlyssonRPG <alyssonrpg@gmail.com>
2019-04-16 12:06:58 -0300

    1 - Modificações para o instalador conseguir usar as distribuições do repositório do WineHQ se o usuário já tiver preparado o computador para isso 2 - O instalador será mais silencioso, mostrando apenas progressos mais importantes.

--- Tools/RRPGWineInstaller/install_rrpg.sh
+++ Tools/RRPGWineInstaller/install_rrpg.sh
@@ -5,77 +5,220 @@ export WINEARCH=win32
 export WINEPREFIX=$HOME/.wine-rrpg
 
 # Constantes
+SILENT=true
+
 RRPG_INSTALLER_NAME=RRPG7.8c_beta3.exe
 RRPG_INSTALLER_URL=http://firecast.app/downloads/$RRPG_INSTALLER_NAME
 RRPG_INSTALL_DIRECTORY=$WINEPREFIX/drive_c/RRPG
 
+LOG_ERROR_PREFIX="\e[31m"  ## red color
+LOG_WARNING_PREFIX="\e[33m"  ## yellow color
+LOG_PROGRESS_COLOR_PREFIX="\e[32m"  ## green color
+LOG_RESET="\e[0m"
+
 ## Funções
+
+prepare_installer_environment_for_silence()
+{
+   if [ "$SILENT" = true ]; then
+     export WINEDEBUG="warn-all,fixme-all,trace-all,err-all"
+     SILENT_Q_FLAG="-q"
+   else
+     export WINEDEBUG=
+     SILENT_Q_FLAG=
+   fi
+}
+
+log_progress()
+{
+  echo "$LOG_PROGRESS_COLOR_PREFIX > $1 $LOG_RESET"
+  return 0
+}
+
+log_warning()
+{
+  echo "$LOG_WARNING_PREFIX > Warning: $1 $LOG_PROGRESS_COLOR_RESET"
+  return 0
+}
+
+log_error()
+{
+  echo "$LOG_ERROR_PREFIX > Error: $1 $LOG_PROGRESS_COLOR_RESET"
+  return 0
+}
+
+apt_get_part_number_of_version_with_grep()
+{
+    TEMP=$(apt-cache show $1 --no-all-versions | grep -oiP "$3")
+
+    if [ -z "$TEMP" ]; then
+      TEMP=0
+    fi
+
+    eval "$2=$TEMP"
+}
 
 apt_get_version()
 {
-    TEMP=$(apt-cache show $1 | grep -oiP "Version:\s*\K([0-9]*)(?=)")
-    eval "$2=$TEMP"
+    apt_get_part_number_of_version_with_grep $1	TEMP_MAJOR "Version:\s*\K([0-9]*)(?=)"
+    apt_get_part_number_of_version_with_grep $1	TEMP_MINOR "Version:\s*([0-9]*)\.\K([0-9]*)(?=)"
 
-    TEMP=$(apt-cache show $1 | grep -oiP "Version:\s*([0-9]*)\.\K([0-9]*)(?=)")
-    eval "$3=$TEMP"
+    eval "$2=$TEMP_MAJOR"
+    eval "$3=$TEMP_MINOR"
 }
 
-choose_wine_stable_or_dev()
+select_wine_package_if_newer_version()
 {
-    apt_get_version wine STABLE_MAJOR_VERSION STABLE_MINOR_VERSION
-    apt_get_version wine-development DEV_MAJOR_VERSION DEV_MINOR_VERSION
+    apt_get_version $1 THIS_PACKAGE_MAJOR_VERSION THIS_PACKAGE_MINOR_VERSION
 
-    if [ $STABLE_MAJOR_VERSION -gt $DEV_MAJOR_VERSION ]; then
-        WINE=wine
-        return
-    fi
-
-    if [ $STABLE_MAJOR_VERSION -lt $DEV_MAJOR_VERSION ]; then
-      WINE=wine-development
+    if [ -z "$THIS_PACKAGE_MAJOR_VERSION" -a -z "$THIS_PACKAGE_MINOR_VERSION" ]; then
+      # not avaliable package
       return
     fi
 
-    if [ $STABLE_MINOR_VERSION -gt $DEV_MINOR_VERSION ]; then
-        WINE=wine
+    is_this_wine_package_newer=false
+
+    if [ $THIS_PACKAGE_MAJOR_VERSION -gt $WINE_PACKAGE_MAJOR_VERSION ]; then
+       is_this_wine_package_newer=true
     else
-        WINE=wine-development
+        if [ $THIS_PACKAGE_MAJOR_VERSION -eq $WINE_PACKAGE_MAJOR_VERSION -a \
+             $THIS_PACKAGE_MINOR_VERSION -gt $WINE_PACKAGE_MINOR_VERSION ]; then
+            is_this_wine_package_newer=true
+        fi
+    fi
+
+    if [ "$is_this_wine_package_newer" = true ]; then
+
+       silent_eval "apt-get install $1 --simulate"
+
+        if [ $? -ne 0 ]; then
+            log_warning "Wine package $1 $THIS_PACKAGE_MAJOR_VERSION.$THIS_PACKAGE_MINOR_VERSION is avaliable but can not be installed"
+	else
+            WINE_PACKAGE="$1"
+            WINE_COMMAND="$2"
+     	    WINE_PACKAGE_MAJOR_VERSION="$THIS_PACKAGE_MAJOR_VERSION"
+	    WINE_PACKAGE_MINOR_VERSION="$THIS_PACKAGE_MINOR_VERSION"
+        fi
     fi
 }
 
-# Script para definir parâmetros
+choose_newer_avaliable_wine_package_to_use()
+{
+    log_progress "Updating apt packages..."	
+    silent_eval "sudo apt-get update"
+
+    WINE_PACKAGE=""
+    WINE_COMMAND=""
+    WINE_PACKAGE_MAJOR_VERSION=0
+    WINE_PACKAGE_MINOR_VERSION=0  
+    
+    log_progress "Selecting Wine package to use..."    
+
+    select_wine_package_if_newer_version "wine-development" "wine-development"
+    select_wine_package_if_newer_version "wine" "wine"
+    select_wine_package_if_newer_version "winehq-stable" "wine"
+    select_wine_package_if_newer_version "winehq-devel" "wine"
+
+    echo ""
+    log_progress "Choosen Wine Package: $WINE_PACKAGE, Command: $WINE_COMMAND, Version: $WINE_PACKAGE_MAJOR_VERSION.$WINE_PACKAGE_MINOR_VERSION"
+    echo ""
+}
+
+prepare_installer_environment()
+{
+   export WINE="$WINE_COMMAND"
+
+   if [ $WINE_PACKAGE_MAJOR_VERSION -ge 4 ]; then
+       # suppress wine 4.0+ requesting wine-mono and wine-gecko dialog	
+       WINE_FIRST_PREFIX_BOOT_ARGS="WINEDLLOVERRIDES=\"mscoree,mshtml=\""
+   fi
+}
+
+silent_eval()
+{
+   if [ "$SILENT" = true ]; then
+     DUMMY=$(eval $1)
+   else
+     eval $1
+   fi
+ 
+  return $?
+}
+
+silent_eval_with_error_care()
+{
+   if [ "$SILENT" = true ]; then
+     DUMMY=$(eval $1)
+
+     if [ $? -ne 0 ]; then
+     	log_error "Error with $1:\n$DUMMY"
+     fi
+   else
+     eval $1
+   fi
+ 
+  return $?
+}
+
+# Script preparatório
+
+prepare_installer_environment_for_silence
 
 if [ $# -eq 1 -a "$1" = "--wd" ]; then
-    export WINE=wine-development
+    WINE_PACKAGE=wine-development
+    WINE_COMMAND=wine-development
 else
-    sudo apt-get update
-    choose_wine_stable_or_dev
+    choose_newer_avaliable_wine_package_to_use
 fi
 
+prepare_installer_environment
+
+
 #instalação do wine, winetricks e winbind (requerido)
-sudo apt-get install -y $WINE winetricks winbind imagemagick #libvulkan1 libvulkan1:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386
+
+set e-
+
+log_progress "Installing apt packages..."
+
+silent_eval_with_error_care "sudo apt-get install -y --install-recommends $WINE_PACKAGE winetricks winbind imagemagick"
 
 #criação e configuração do prefix 32bits exclusivo pro RRPG
+log_progress "Preparing wine prefix..."
+
 rm -rf $WINEPREFIX
-eval $WINE wineboot
-eval winetricks -q gdiplus ole32 windowscodecs wininet mfc42 riched30 d3dx11_43 #comctl32 comctl32ocx comdlg32ocx allfonts
-eval winetricks -q win10
+silent_eval_with_error_care "$WINE_FIRST_PREFIX_BOOT_ARGS $WINE wineboot"
+
+log_progress "Installing winetricks components..."
+
+silent_eval_with_error_care "winetricks -q gdiplus ole32 windowscodecs wininet mfc42 riched30 d3dx11_43"
+silent_eval_with_error_care "winetricks -q win10"
 
 #download do instalador
+
 if [ ! -f ./$RRPG_INSTALLER_NAME ]; then
-    wget $RRPG_INSTALLER_URL -O ./$RRPG_INSTALLER_NAME
+    log_progress "Downloading RRPG installer..." 
+    silent_eval_with_error_care "wget $SILENT_Q_FLAG $RRPG_INSTALLER_URL -O ./$RRPG_INSTALLER_NAME"
 fi
 
 #start do instalador do RRPG
-eval $WINE ./$RRPG_INSTALLER_NAME /VERYSILENT /WINE
+
+log_progress "Installing RRPG..."
+silent_eval_with_error_care "$WINE ./$RRPG_INSTALLER_NAME /VERYSILENT /WINE"
+
+set e+
 
 #Instalação das fontes
-sudo mkdir /usr/local/share/fonts/ms_fonts
-sudo cp $RRPG_INSTALL_DIRECTORY/wine/fonts/*.ttf /usr/local/share/fonts/ms_fonts/
-sudo chmod 644 /usr/local/share/fonts/ms_fonts/* -R
-sudo chmod 755 /usr/local/share/fonts/ms_fonts
-sudo fc-cache -fv
+log_progress "Installing Fonts..."
+
+silent_eval "sudo mkdir -p /usr/local/share/fonts/ms_fonts"
+silent_eval "sudo cp $RRPG_INSTALL_DIRECTORY/wine/fonts/*.ttf /usr/local/share/fonts/ms_fonts/"
+silent_eval "sudo chmod 644 /usr/local/share/fonts/ms_fonts/* -R"
+silent_eval "sudo chmod 755 /usr/local/share/fonts/ms_fonts"
+silent_eval "sudo fc-cache -fv"
 
 #criação do script de execução do RRPG
+log_progress "Installing RRPG shortcuts..."
+
 cat <<EOF >$RRPG_INSTALL_DIRECTORY/wine/rrpg.sh
 #!/bin/sh
 export WINEARCH=$WINEARCH
@@ -83,17 +226,17 @@ export WINEPREFIX=$WINEPREFIX
 $WINE $RRPG_INSTALL_DIRECTORY/rrpg.exe
 EOF
 
-chmod +x $RRPG_INSTALL_DIRECTORY/wine/rrpg.sh
-ln -sfn $RRPG_INSTALL_DIRECTORY/wine/rrpg.sh $HOME/rrpg.sh
+silent_eval_with_error_care "chmod +x $RRPG_INSTALL_DIRECTORY/wine/rrpg.sh"
+silent_eval_with_error_care "ln -sfn $RRPG_INSTALL_DIRECTORY/wine/rrpg.sh $HOME/rrpg.sh"
 
 #adição dos atalhos aos menus
-mkdir -p ~/.local/share/icons/hicolor/256x256/apps/ && cp $RRPG_INSTALL_DIRECTORY/wine/images/rrpg_logo_256.png ~/.local/share/icons/hicolor/256x256/apps/rrpg.png
-mkdir -p ~/.local/share/icons/hicolor/128x128/apps/ && convert $RRPG_INSTALL_DIRECTORY/wine/images/rrpg_logo_256.png -resize 128x128 ~/.local/share/icons/hicolor/128x128/apps/rrpg.png
-mkdir -p ~/.local/share/icons/hicolor/64x64/apps/ && convert $RRPG_INSTALL_DIRECTORY/wine/images/rrpg_logo_256.png -resize 64x64 ~/.local/share/icons/hicolor/64x64/apps/rrpg.png
-mkdir -p ~/.local/share/icons/hicolor/48x48/apps/ && convert $RRPG_INSTALL_DIRECTORY/wine/images/rrpg_logo_256.png -resize 48x48 ~/.local/share/icons/hicolor/48x48/apps/rrpg.png
-mkdir -p ~/.local/share/icons/hicolor/32x32/apps/ && convert $RRPG_INSTALL_DIRECTORY/wine/images/rrpg_logo_256.png -resize 32x32 ~/.local/share/icons/hicolor/32x32/apps/rrpg.png
-mkdir -p ~/.local/share/icons/hicolor/24x24/apps/ && convert $RRPG_INSTALL_DIRECTORY/wine/images/rrpg_logo_256.png -resize 24x24 ~/.local/share/icons/hicolor/24x24/apps/rrpg.png
-mkdir -p ~/.local/share/icons/hicolor/16x16/apps/ && convert $RRPG_INSTALL_DIRECTORY/wine/images/rrpg_logo_256.png -resize 16x16 ~/.local/share/icons/hicolor/16x16/apps/rrpg.png
+silent_eval "mkdir -p ~/.local/share/icons/hicolor/256x256/apps/ && cp $RRPG_INSTALL_DIRECTORY/wine/images/rrpg_logo_256.png ~/.local/share/icons/hicolor/256x256/apps/rrpg.png"
+silent_eval "mkdir -p ~/.local/share/icons/hicolor/128x128/apps/ && convert $RRPG_INSTALL_DIRECTORY/wine/images/rrpg_logo_256.png -resize 128x128 ~/.local/share/icons/hicolor/128x128/apps/rrpg.png"
+silent_eval "mkdir -p ~/.local/share/icons/hicolor/64x64/apps/ && convert $RRPG_INSTALL_DIRECTORY/wine/images/rrpg_logo_256.png -resize 64x64 ~/.local/share/icons/hicolor/64x64/apps/rrpg.png"
+silent_eval "mkdir -p ~/.local/share/icons/hicolor/48x48/apps/ && convert $RRPG_INSTALL_DIRECTORY/wine/images/rrpg_logo_256.png -resize 48x48 ~/.local/share/icons/hicolor/48x48/apps/rrpg.png"
+silent_eval "mkdir -p ~/.local/share/icons/hicolor/32x32/apps/ && convert $RRPG_INSTALL_DIRECTORY/wine/images/rrpg_logo_256.png -resize 32x32 ~/.local/share/icons/hicolor/32x32/apps/rrpg.png"
+silent_eval "mkdir -p ~/.local/share/icons/hicolor/24x24/apps/ && convert $RRPG_INSTALL_DIRECTORY/wine/images/rrpg_logo_256.png -resize 24x24 ~/.local/share/icons/hicolor/24x24/apps/rrpg.png"
+silent_eval "mkdir -p ~/.local/share/icons/hicolor/16x16/apps/ && convert $RRPG_INSTALL_DIRECTORY/wine/images/rrpg_logo_256.png -resize 16x16 ~/.local/share/icons/hicolor/16x16/apps/rrpg.png"
 
 cat <<EOF >$RRPG_INSTALL_DIRECTORY/wine/rrpg.desktop
 [Desktop Entry]
@@ -106,11 +249,12 @@ Name[en_US]=RRPG Firecast
 Categories=Game
 EOF
 
-desktop-file-install --dir=$HOME/.local/share/applications $RRPG_INSTALL_DIRECTORY/wine/rrpg.desktop
+silent_eval "desktop-file-install --dir=$HOME/.local/share/applications $RRPG_INSTALL_DIRECTORY/wine/rrpg.desktop"
 
 rm ./$RRPG_INSTALLER_NAME
 
 echo ""
 echo ""
-echo "RRPG Instalado com sucesso! Procure por RRPG Firecast no menu \"Jogos\" ou \"Games\" do seu ambiente gráfico!"
+log_progress "=) RRPG Instalado com sucesso! Procure por RRPG Firecast no menu \"Jogos\" ou \"Games\" do seu ambiente gráfico!"
+log_progress "Também é possível executá-lo no terminal com o comando ~/rrpg.sh"
 echo ""
