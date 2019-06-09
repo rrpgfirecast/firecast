d71b8fecbb87199ee9df2d360c65cd299f24110c
VinnyDiniz <vinicius.lima@manifestohut.com.br>
2019-06-09 14:29:10 -0300

    Ficha the Maza v3.0 * Atualização do sistema da ficha. * Tracker de Combate para o sistema.

--- Plugins/Sheets/Ficha The Maze/output/rdkObjs/TheMaze/Inventory.lfm.lua
+++ Plugins/Sheets/Ficha The Maze/output/rdkObjs/TheMaze/Inventory.lfm.lua
@@ -39,7 +39,7 @@ local function constructNew_frmInventory()
     obj.rectangle1:setLeft(0);
     obj.rectangle1:setTop(0);
     obj.rectangle1:setWidth(895);
-    obj.rectangle1:setHeight(110);
+    obj.rectangle1:setHeight(160);
     obj.rectangle1:setColor("black");
     obj.rectangle1:setStrokeColor("white");
     obj.rectangle1:setStrokeSize(1);
@@ -47,23 +47,33 @@ local function constructNew_frmInventory()
 
     obj.label1 = GUI.fromHandle(_obj_newObject("label"));
     obj.label1:setParent(obj.rectangle1);
-    obj.label1:setLeft(0);
+    obj.label1:setLeft(100);
     obj.label1:setTop(5);
-    obj.label1:setWidth(895);
+    obj.label1:setWidth(200);
     obj.label1:setHeight(25);
     obj.label1:setHorzTextAlign("center");
-    obj.label1:setText("Equipados");
+    obj.label1:setText("Itens Equipados");
     obj.label1:setName("label1");
 
     obj.label2 = GUI.fromHandle(_obj_newObject("label"));
     obj.label2:setParent(obj.rectangle1);
-    obj.label2:setLeft(5);
-    obj.label2:setTop(30);
-    obj.label2:setWidth(95);
-    obj.label2:setHeight(20);
-    obj.label2:setText("Arma");
+    obj.label2:setLeft(300);
+    obj.label2:setTop(5);
+    obj.label2:setWidth(590);
+    obj.label2:setHeight(25);
     obj.label2:setHorzTextAlign("center");
+    obj.label2:setText("Efeitos Adicionais");
     obj.label2:setName("label2");
+
+    obj.label3 = GUI.fromHandle(_obj_newObject("label"));
+    obj.label3:setParent(obj.rectangle1);
+    obj.label3:setLeft(5);
+    obj.label3:setTop(30);
+    obj.label3:setWidth(95);
+    obj.label3:setHeight(20);
+    obj.label3:setText("Arma");
+    obj.label3:setHorzTextAlign("center");
+    obj.label3:setName("label3");
 
     obj.edit1 = GUI.fromHandle(_obj_newObject("edit"));
     obj.edit1:setParent(obj.rectangle1);
@@ -85,15 +95,15 @@ local function constructNew_frmInventory()
     obj.edit2:setVertTextAlign("center");
     obj.edit2:setName("edit2");
 
-    obj.label3 = GUI.fromHandle(_obj_newObject("label"));
-    obj.label3:setParent(obj.rectangle1);
-    obj.label3:setLeft(5);
-    obj.label3:setTop(55);
-    obj.label3:setWidth(95);
-    obj.label3:setHeight(20);
-    obj.label3:setText("Equipamento");
-    obj.label3:setHorzTextAlign("center");
-    obj.label3:setName("label3");
+    obj.label4 = GUI.fromHandle(_obj_newObject("label"));
+    obj.label4:setParent(obj.rectangle1);
+    obj.label4:setLeft(5);
+    obj.label4:setTop(55);
+    obj.label4:setWidth(95);
+    obj.label4:setHeight(20);
+    obj.label4:setText("Equipamento");
+    obj.label4:setHorzTextAlign("center");
+    obj.label4:setName("label4");
 
     obj.edit3 = GUI.fromHandle(_obj_newObject("edit"));
     obj.edit3:setParent(obj.rectangle1);
@@ -115,15 +125,15 @@ local function constructNew_frmInventory()
     obj.edit4:setVertTextAlign("center");
     obj.edit4:setName("edit4");
 
-    obj.label4 = GUI.fromHandle(_obj_newObject("label"));
-    obj.label4:setParent(obj.rectangle1);
-    obj.label4:setLeft(5);
-    obj.label4:setTop(80);
-    obj.label4:setWidth(95);
-    obj.label4:setHeight(20);
-    obj.label4:setText("Vestimenta");
-    obj.label4:setHorzTextAlign("center");
-    obj.label4:setName("label4");
+    obj.label5 = GUI.fromHandle(_obj_newObject("label"));
+    obj.label5:setParent(obj.rectangle1);
+    obj.label5:setLeft(5);
+    obj.label5:setTop(80);
+    obj.label5:setWidth(95);
+    obj.label5:setHeight(20);
+    obj.label5:setText("Vestimenta");
+    obj.label5:setHorzTextAlign("center");
+    obj.label5:setName("label5");
 
     obj.edit5 = GUI.fromHandle(_obj_newObject("edit"));
     obj.edit5:setParent(obj.rectangle1);
@@ -145,6 +155,26 @@ local function constructNew_frmInventory()
     obj.edit6:setVertTextAlign("center");
     obj.edit6:setName("edit6");
 
+    obj.edit7 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit7:setParent(obj.rectangle1);
+    obj.edit7:setLeft(100);
+    obj.edit7:setTop(105);
+    obj.edit7:setWidth(200);
+    obj.edit7:setHeight(25);
+    obj.edit7:setField("equip_vestimenta1");
+    obj.edit7:setVertTextAlign("center");
+    obj.edit7:setName("edit7");
+
+    obj.edit8 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit8:setParent(obj.rectangle1);
+    obj.edit8:setLeft(100);
+    obj.edit8:setTop(130);
+    obj.edit8:setWidth(200);
+    obj.edit8:setHeight(25);
+    obj.edit8:setField("equip_vestimenta2");
+    obj.edit8:setVertTextAlign("center");
+    obj.edit8:setName("edit8");
+
     obj.rectangle2 = GUI.fromHandle(_obj_newObject("rectangle"));
     obj.rectangle2:setParent(obj.scrollBox1);
     obj.rectangle2:setLeft(900);
@@ -156,15 +186,15 @@ local function constructNew_frmInventory()
     obj.rectangle2:setStrokeSize(1);
     obj.rectangle2:setName("rectangle2");
 
-    obj.label5 = GUI.fromHandle(_obj_newObject("label"));
-    obj.label5:setParent(obj.rectangle2);
-    obj.label5:setLeft(0);
-    obj.label5:setTop(5);
-    obj.label5:setWidth(310);
-    obj.label5:setHeight(25);
-    obj.label5:setHorzTextAlign("center");
-    obj.label5:setText("Armazenamento");
-    obj.label5:setName("label5");
+    obj.label6 = GUI.fromHandle(_obj_newObject("label"));
+    obj.label6:setParent(obj.rectangle2);
+    obj.label6:setLeft(0);
+    obj.label6:setTop(5);
+    obj.label6:setWidth(310);
+    obj.label6:setHeight(25);
+    obj.label6:setHorzTextAlign("center");
+    obj.label6:setText("Armazenamento");
+    obj.label6:setName("label6");
 
     obj.comboBox1 = GUI.fromHandle(_obj_newObject("comboBox"));
     obj.comboBox1:setParent(obj.rectangle2);
@@ -207,70 +237,70 @@ local function constructNew_frmInventory()
     obj.rectangle3 = GUI.fromHandle(_obj_newObject("rectangle"));
     obj.rectangle3:setParent(obj.scrollBox1);
     obj.rectangle3:setLeft(0);
-    obj.rectangle3:setTop(115);
+    obj.rectangle3:setTop(165);
     obj.rectangle3:setWidth(1210);
-    obj.rectangle3:setHeight(555);
+    obj.rectangle3:setHeight(505);
     obj.rectangle3:setColor("black");
     obj.rectangle3:setStrokeColor("white");
     obj.rectangle3:setStrokeSize(1);
     obj.rectangle3:setName("rectangle3");
 
-    obj.label6 = GUI.fromHandle(_obj_newObject("label"));
-    obj.label6:setParent(obj.rectangle3);
-    obj.label6:setLeft(5);
-    obj.label6:setTop(5);
-    obj.label6:setWidth(200);
-    obj.label6:setHeight(25);
-    obj.label6:setHorzTextAlign("center");
-    obj.label6:setText("Nome");
-    obj.label6:setName("label6");
-
     obj.label7 = GUI.fromHandle(_obj_newObject("label"));
     obj.label7:setParent(obj.rectangle3);
-    obj.label7:setLeft(205);
+    obj.label7:setLeft(5);
     obj.label7:setTop(5);
-    obj.label7:setWidth(700);
+    obj.label7:setWidth(200);
     obj.label7:setHeight(25);
     obj.label7:setHorzTextAlign("center");
-    obj.label7:setText("Efeito");
+    obj.label7:setText("Itens Guardados");
     obj.label7:setName("label7");
 
     obj.label8 = GUI.fromHandle(_obj_newObject("label"));
     obj.label8:setParent(obj.rectangle3);
-    obj.label8:setLeft(905);
+    obj.label8:setLeft(205);
     obj.label8:setTop(5);
-    obj.label8:setWidth(150);
+    obj.label8:setWidth(700);
     obj.label8:setHeight(25);
     obj.label8:setHorzTextAlign("center");
-    obj.label8:setText("Tipo");
+    obj.label8:setText("Efeitos Adicionais");
     obj.label8:setName("label8");
 
     obj.label9 = GUI.fromHandle(_obj_newObject("label"));
     obj.label9:setParent(obj.rectangle3);
-    obj.label9:setLeft(1055);
+    obj.label9:setLeft(905);
     obj.label9:setTop(5);
-    obj.label9:setWidth(50);
+    obj.label9:setWidth(150);
     obj.label9:setHeight(25);
     obj.label9:setHorzTextAlign("center");
-    obj.label9:setText("Qtd");
+    obj.label9:setText("Tipo");
     obj.label9:setName("label9");
 
     obj.label10 = GUI.fromHandle(_obj_newObject("label"));
     obj.label10:setParent(obj.rectangle3);
-    obj.label10:setLeft(1105);
+    obj.label10:setLeft(1055);
     obj.label10:setTop(5);
     obj.label10:setWidth(50);
     obj.label10:setHeight(25);
     obj.label10:setHorzTextAlign("center");
-    obj.label10:setText("Max");
+    obj.label10:setText("Qtd");
     obj.label10:setName("label10");
+
+    obj.label11 = GUI.fromHandle(_obj_newObject("label"));
+    obj.label11:setParent(obj.rectangle3);
+    obj.label11:setLeft(1105);
+    obj.label11:setTop(5);
+    obj.label11:setWidth(50);
+    obj.label11:setHeight(25);
+    obj.label11:setHorzTextAlign("center");
+    obj.label11:setText("Max");
+    obj.label11:setName("label11");
 
     obj.rclInventario = GUI.fromHandle(_obj_newObject("recordList"));
     obj.rclInventario:setParent(obj.rectangle3);
     obj.rclInventario:setLeft(5);
     obj.rclInventario:setTop(30);
     obj.rclInventario:setWidth(1200);
-    obj.rclInventario:setHeight(520);
+    obj.rclInventario:setHeight(470);
     obj.rclInventario:setName("rclInventario");
     obj.rclInventario:setField("listaDeItens");
     obj.rclInventario:setTemplateForm("frmItem");
@@ -312,6 +342,7 @@ local function constructNew_frmInventory()
         if self.label1 ~= nil then self.label1:destroy(); self.label1 = nil; end;
         if self.edit4 ~= nil then self.edit4:destroy(); self.edit4 = nil; end;
         if self.label8 ~= nil then self.label8:destroy(); self.label8 = nil; end;
+        if self.label11 ~= nil then self.label11:destroy(); self.label11 = nil; end;
         if self.label3 ~= nil then self.label3:destroy(); self.label3 = nil; end;
         if self.label4 ~= nil then self.label4:destroy(); self.label4 = nil; end;
         if self.label6 ~= nil then self.label6:destroy(); self.label6 = nil; end;
@@ -319,10 +350,12 @@ local function constructNew_frmInventory()
         if self.comboBox1 ~= nil then self.comboBox1:destroy(); self.comboBox1 = nil; end;
         if self.edit6 ~= nil then self.edit6:destroy(); self.edit6 = nil; end;
         if self.label7 ~= nil then self.label7:destroy(); self.label7 = nil; end;
+        if self.edit7 ~= nil then self.edit7:destroy(); self.edit7 = nil; end;
         if self.dataLink1 ~= nil then self.dataLink1:destroy(); self.dataLink1 = nil; end;
         if self.label2 ~= nil then self.label2:destroy(); self.label2 = nil; end;
         if self.edit3 ~= nil then self.edit3:destroy(); self.edit3 = nil; end;
         if self.label5 ~= nil then self.label5:destroy(); self.label5 = nil; end;
+        if self.edit8 ~= nil then self.edit8:destroy(); self.edit8 = nil; end;
         if self.rectangle2 ~= nil then self.rectangle2:destroy(); self.rectangle2 = nil; end;
         if self.rectangle3 ~= nil then self.rectangle3:destroy(); self.rectangle3 = nil; end;
         if self.rclInventario ~= nil then self.rclInventario:destroy(); self.rclInventario = nil; end;
