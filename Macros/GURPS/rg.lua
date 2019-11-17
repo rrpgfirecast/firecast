a9ed8b5df33f096548ac313e91babc5b7ebb5df4
VinnyAmbesek <vinnyrpgmeister@gmail.com>
2019-11-17 19:55:40 -0300

    Update and rename rg.txt to rg.lua

--- Macros/GURPS/rg.lua
+++ Macros/GURPS/rg.lua
@@ -1,17 +1,18 @@
 -- Faz uma rolagem de 3d6 e compara o valor com o NH
 local dificuldade = tonumber(parametro); 
-if dificuldade == nil then
-   escrever("Utilize: /rg <NH>");
-   return;
-end; 
+while dificuldade == nil do
+   dificuldade = tonumber(inputQuery("NH:"));
+end;
 
 local resultado = rolar("3d6", "NH " .. dificuldade);
+local diff = math.abs(dificuldade - resultado);
+
 if resultado==18 or (resultado>=17 and dificuldade<=15) or resultado-10>=dificuldade then
-  enviar("Falha Critica!");
+  enviar("Falha Critica por ".. diff .."!");
 elseif resultado<=4 or (resultado<=5 and dificuldade>=15) or (resultado<=6 and dificuldade>=16) then
-  enviar("Sucesso Critico!");
+  enviar("Sucesso Critico por ".. diff .."!");
 elseif resultado > dificuldade then
-  enviar("Falha!");
+  enviar("Falha por ".. diff .."!");
 else
-  enviar("Sucesso");
+  enviar("Sucesso por ".. diff .."!");
 end;
