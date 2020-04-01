9f2388b415466c6d662f66e48dc8ddfca919f9f9
VinnyDiniz <vinicius.lima@manifestohut.com.br>
2020-04-01 20:39:03 -0300

    Ficha A Odisseia do Dragão v2.0

---
+++ Plugins/Sheets/FichaAODD/output/rdkObjs/FichaOdisseia2.0/1.lfm.lua
@@ -0,0 +1,524 @@
+require("firecast.lua");
+local __o_rrpgObjs = require("rrpgObjs.lua");
+require("rrpgGUI.lua");
+require("rrpgDialogs.lua");
+require("rrpgLFM.lua");
+require("ndb.lua");
+require("locale.lua");
+local __o_Utils = require("utils.lua");
+
+local function constructNew_frmFichaOdisseia2_01_svg()
+    local obj = GUI.fromHandle(_obj_newObject("form"));
+    local self = obj;
+    local sheet = nil;
+
+    rawset(obj, "_oldSetNodeObjectFunction", rawget(obj, "setNodeObject"));
+
+    function obj:setNodeObject(nodeObject)
+        sheet = nodeObject;
+        self.sheet = nodeObject;
+        self:_oldSetNodeObjectFunction(nodeObject);
+    end;
+
+    function obj:setNodeDatabase(nodeObject)
+        self:setNodeObject(nodeObject);
+    end;
+
+    _gui_assignInitialParentForForm(obj.handle);
+    obj:beginUpdate();
+    obj:setName("frmFichaOdisseia2_01_svg");
+    obj:setAlign("client");
+    obj:setTheme("light");
+    obj:setMargins({top=1});
+
+    obj.scrollBox1 = GUI.fromHandle(_obj_newObject("scrollBox"));
+    obj.scrollBox1:setParent(obj);
+    obj.scrollBox1:setAlign("client");
+    obj.scrollBox1:setName("scrollBox1");
+
+    obj.rectangle1 = GUI.fromHandle(_obj_newObject("rectangle"));
+    obj.rectangle1:setParent(obj.scrollBox1);
+    obj.rectangle1:setWidth(952);
+    obj.rectangle1:setHeight(1347);
+    obj.rectangle1:setColor("white");
+    obj.rectangle1:setName("rectangle1");
+
+    obj.image1 = GUI.fromHandle(_obj_newObject("image"));
+    obj.image1:setParent(obj.rectangle1);
+    obj.image1:setLeft(0);
+    obj.image1:setTop(0);
+    obj.image1:setWidth(952);
+    obj.image1:setHeight(1347);
+    obj.image1:setSRC("/FichaOdisseia2.0/images/1.png");
+    obj.image1:setStyle("stretch");
+    obj.image1:setOptimize(true);
+    obj.image1:setName("image1");
+
+    obj.layout1 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout1:setParent(obj.rectangle1);
+    obj.layout1:setLeft(172);
+    obj.layout1:setTop(492);
+    obj.layout1:setWidth(232);
+    obj.layout1:setHeight(40);
+    obj.layout1:setName("layout1");
+
+    obj.edit1 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit1:setParent(obj.layout1);
+    obj.edit1:setTransparent(true);
+    obj.edit1:setFontSize(20.8);
+    obj.edit1:setFontColor("#8B0000");
+    obj.edit1:setHorzTextAlign("center");
+    obj.edit1:setVertTextAlign("center");
+    obj.edit1:setLeft(0);
+    obj.edit1:setTop(0);
+    obj.edit1:setWidth(232);
+    obj.edit1:setHeight(41);
+    obj.edit1:setField("NomedoPersonagem");
+    obj.edit1:setName("edit1");
+
+    obj.layout2 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout2:setParent(obj.rectangle1);
+    obj.layout2:setLeft(463);
+    obj.layout2:setTop(492);
+    obj.layout2:setWidth(223);
+    obj.layout2:setHeight(41);
+    obj.layout2:setName("layout2");
+
+    obj.edit2 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit2:setParent(obj.layout2);
+    obj.edit2:setTransparent(true);
+    obj.edit2:setFontSize(20.8);
+    obj.edit2:setFontColor("#8B0000");
+    obj.edit2:setHorzTextAlign("center");
+    obj.edit2:setVertTextAlign("center");
+    obj.edit2:setLeft(0);
+    obj.edit2:setTop(0);
+    obj.edit2:setWidth(223);
+    obj.edit2:setHeight(42);
+    obj.edit2:setField("RaçadoPersonagem");
+    obj.edit2:setName("edit2");
+
+    obj.layout3 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout3:setParent(obj.rectangle1);
+    obj.layout3:setLeft(173);
+    obj.layout3:setTop(573);
+    obj.layout3:setWidth(226);
+    obj.layout3:setHeight(40);
+    obj.layout3:setName("layout3");
+
+    obj.edit3 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit3:setParent(obj.layout3);
+    obj.edit3:setTransparent(true);
+    obj.edit3:setFontSize(20.8);
+    obj.edit3:setFontColor("#8B0000");
+    obj.edit3:setHorzTextAlign("center");
+    obj.edit3:setVertTextAlign("center");
+    obj.edit3:setLeft(0);
+    obj.edit3:setTop(0);
+    obj.edit3:setWidth(226);
+    obj.edit3:setHeight(41);
+    obj.edit3:setField("NomedoJogador");
+    obj.edit3:setName("edit3");
+
+    obj.layout4 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout4:setParent(obj.rectangle1);
+    obj.layout4:setLeft(461);
+    obj.layout4:setTop(573);
+    obj.layout4:setWidth(224);
+    obj.layout4:setHeight(41);
+    obj.layout4:setName("layout4");
+
+    obj.edit4 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit4:setParent(obj.layout4);
+    obj.edit4:setTransparent(true);
+    obj.edit4:setFontSize(20.8);
+    obj.edit4:setFontColor("#8B0000");
+    obj.edit4:setHorzTextAlign("center");
+    obj.edit4:setVertTextAlign("center");
+    obj.edit4:setLeft(0);
+    obj.edit4:setTop(0);
+    obj.edit4:setWidth(224);
+    obj.edit4:setHeight(42);
+    obj.edit4:setField("ClassedoPersonagem");
+    obj.edit4:setName("edit4");
+
+    obj.layout5 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout5:setParent(obj.rectangle1);
+    obj.layout5:setLeft(743);
+    obj.layout5:setTop(516);
+    obj.layout5:setWidth(66);
+    obj.layout5:setHeight(47);
+    obj.layout5:setName("layout5");
+
+    obj.edit5 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit5:setParent(obj.layout5);
+    obj.edit5:setTransparent(true);
+    obj.edit5:setFontSize(30.8);
+    obj.edit5:setFontColor("#8B0000");
+    lfm_setPropAsString(obj.edit5, "fontStyle",  "bold");
+    obj.edit5:setHorzTextAlign("center");
+    obj.edit5:setVertTextAlign("center");
+    obj.edit5:setLeft(0);
+    obj.edit5:setTop(0);
+    obj.edit5:setWidth(66);
+    obj.edit5:setHeight(48);
+    obj.edit5:setField("NíveldoPersonagem");
+    obj.edit5:setName("edit5");
+
+    obj.layout6 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout6:setParent(obj.rectangle1);
+    obj.layout6:setLeft(246);
+    obj.layout6:setTop(828);
+    obj.layout6:setWidth(46);
+    obj.layout6:setHeight(37);
+    obj.layout6:setName("layout6");
+
+    obj.edit6 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit6:setParent(obj.layout6);
+    obj.edit6:setTransparent(true);
+    obj.edit6:setFontSize(20.8);
+    obj.edit6:setFontColor("#8B0000");
+    lfm_setPropAsString(obj.edit6, "fontStyle",  "bold");
+    obj.edit6:setHorzTextAlign("center");
+    obj.edit6:setVertTextAlign("center");
+    obj.edit6:setLeft(0);
+    obj.edit6:setTop(0);
+    obj.edit6:setWidth(44);
+    obj.edit6:setHeight(38);
+    obj.edit6:setField("Const");
+    obj.edit6:setName("edit6");
+
+    obj.layout7 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout7:setParent(obj.rectangle1);
+    obj.layout7:setLeft(349);
+    obj.layout7:setTop(870);
+    obj.layout7:setWidth(46);
+    obj.layout7:setHeight(37);
+    obj.layout7:setName("layout7");
+
+    obj.edit7 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit7:setParent(obj.layout7);
+    obj.edit7:setTransparent(true);
+    obj.edit7:setFontSize(20.8);
+    obj.edit7:setFontColor("#8B0000");
+    lfm_setPropAsString(obj.edit7, "fontStyle",  "bold");
+    obj.edit7:setHorzTextAlign("center");
+    obj.edit7:setVertTextAlign("center");
+    obj.edit7:setLeft(0);
+    obj.edit7:setTop(0);
+    obj.edit7:setWidth(44);
+    obj.edit7:setHeight(38);
+    obj.edit7:setField("Int");
+    obj.edit7:setName("edit7");
+
+    obj.layout8 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout8:setParent(obj.rectangle1);
+    obj.layout8:setLeft(356);
+    obj.layout8:setTop(999);
+    obj.layout8:setWidth(46);
+    obj.layout8:setHeight(37);
+    obj.layout8:setName("layout8");
+
+    obj.edit8 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit8:setParent(obj.layout8);
+    obj.edit8:setTransparent(true);
+    obj.edit8:setFontSize(20.8);
+    obj.edit8:setFontColor("#8B0000");
+    lfm_setPropAsString(obj.edit8, "fontStyle",  "bold");
+    obj.edit8:setHorzTextAlign("center");
+    obj.edit8:setVertTextAlign("center");
+    obj.edit8:setLeft(0);
+    obj.edit8:setTop(0);
+    obj.edit8:setWidth(44);
+    obj.edit8:setHeight(38);
+    obj.edit8:setField("Mist");
+    obj.edit8:setName("edit8");
+
+    obj.layout9 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout9:setParent(obj.rectangle1);
+    obj.layout9:setLeft(241);
+    obj.layout9:setTop(1049);
+    obj.layout9:setWidth(46);
+    obj.layout9:setHeight(37);
+    obj.layout9:setName("layout9");
+
+    obj.edit9 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit9:setParent(obj.layout9);
+    obj.edit9:setTransparent(true);
+    obj.edit9:setFontSize(20.8);
+    obj.edit9:setFontColor("#8B0000");
+    lfm_setPropAsString(obj.edit9, "fontStyle",  "bold");
+    obj.edit9:setHorzTextAlign("center");
+    obj.edit9:setVertTextAlign("center");
+    obj.edit9:setLeft(0);
+    obj.edit9:setTop(0);
+    obj.edit9:setWidth(44);
+    obj.edit9:setHeight(38);
+    obj.edit9:setField("Car");
+    obj.edit9:setName("edit9");
+
+    obj.layout10 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout10:setParent(obj.rectangle1);
+    obj.layout10:setLeft(142);
+    obj.layout10:setTop(999);
+    obj.layout10:setWidth(46);
+    obj.layout10:setHeight(37);
+    obj.layout10:setName("layout10");
+
+    obj.edit10 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit10:setParent(obj.layout10);
+    obj.edit10:setTransparent(true);
+    obj.edit10:setFontSize(20.8);
+    obj.edit10:setFontColor("#8B0000");
+    lfm_setPropAsString(obj.edit10, "fontStyle",  "bold");
+    obj.edit10:setHorzTextAlign("center");
+    obj.edit10:setVertTextAlign("center");
+    obj.edit10:setLeft(0);
+    obj.edit10:setTop(0);
+    obj.edit10:setWidth(44);
+    obj.edit10:setHeight(38);
+    obj.edit10:setField("Dest");
+    obj.edit10:setName("edit10");
+
+    obj.layout11 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout11:setParent(obj.rectangle1);
+    obj.layout11:setLeft(134);
+    obj.layout11:setTop(861);
+    obj.layout11:setWidth(46);
+    obj.layout11:setHeight(37);
+    obj.layout11:setName("layout11");
+
+    obj.edit11 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit11:setParent(obj.layout11);
+    obj.edit11:setTransparent(true);
+    obj.edit11:setFontSize(20.8);
+    obj.edit11:setFontColor("#8B0000");
+    lfm_setPropAsString(obj.edit11, "fontStyle",  "bold");
+    obj.edit11:setHorzTextAlign("center");
+    obj.edit11:setVertTextAlign("center");
+    obj.edit11:setLeft(0);
+    obj.edit11:setTop(0);
+    obj.edit11:setWidth(44);
+    obj.edit11:setHeight(38);
+    obj.edit11:setField("For");
+    obj.edit11:setName("edit11");
+
+    obj.layout12 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout12:setParent(obj.rectangle1);
+    obj.layout12:setLeft(640);
+    obj.layout12:setTop(789);
+    obj.layout12:setWidth(60);
+    obj.layout12:setHeight(28);
+    obj.layout12:setName("layout12");
+
+    obj.edit12 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit12:setParent(obj.layout12);
+    obj.edit12:setTransparent(true);
+    obj.edit12:setFontSize(18.8);
+    obj.edit12:setFontColor("#8B0000");
+    lfm_setPropAsString(obj.edit12, "fontStyle",  "bold");
+    obj.edit12:setHorzTextAlign("center");
+    obj.edit12:setVertTextAlign("center");
+    obj.edit12:setLeft(0);
+    obj.edit12:setTop(0);
+    obj.edit12:setWidth(60);
+    obj.edit12:setHeight(29);
+    obj.edit12:setField("EXP_Atual");
+    obj.edit12:setName("edit12");
+
+    obj.layout13 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout13:setParent(obj.rectangle1);
+    obj.layout13:setLeft(798);
+    obj.layout13:setTop(789);
+    obj.layout13:setWidth(60);
+    obj.layout13:setHeight(28);
+    obj.layout13:setName("layout13");
+
+    obj.edit13 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit13:setParent(obj.layout13);
+    obj.edit13:setTransparent(true);
+    obj.edit13:setFontSize(18.8);
+    obj.edit13:setFontColor("#8B0000");
+    lfm_setPropAsString(obj.edit13, "fontStyle",  "bold");
+    obj.edit13:setHorzTextAlign("center");
+    obj.edit13:setVertTextAlign("center");
+    obj.edit13:setLeft(0);
+    obj.edit13:setTop(0);
+    obj.edit13:setWidth(60);
+    obj.edit13:setHeight(29);
+    obj.edit13:setField("EXPTotal");
+    obj.edit13:setName("edit13");
+
+    obj.layout14 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout14:setParent(obj.rectangle1);
+    obj.layout14:setLeft(640);
+    obj.layout14:setTop(890);
+    obj.layout14:setWidth(60);
+    obj.layout14:setHeight(28);
+    obj.layout14:setName("layout14");
+
+    obj.edit14 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit14:setParent(obj.layout14);
+    obj.edit14:setTransparent(true);
+    obj.edit14:setFontSize(18.8);
+    obj.edit14:setFontColor("#8B0000");
+    lfm_setPropAsString(obj.edit14, "fontStyle",  "bold");
+    obj.edit14:setHorzTextAlign("center");
+    obj.edit14:setVertTextAlign("center");
+    obj.edit14:setLeft(0);
+    obj.edit14:setTop(0);
+    obj.edit14:setWidth(60);
+    obj.edit14:setHeight(29);
+    obj.edit14:setField("PHATual");
+    obj.edit14:setName("edit14");
+
+    obj.layout15 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout15:setParent(obj.rectangle1);
+    obj.layout15:setLeft(790);
+    obj.layout15:setTop(892);
+    obj.layout15:setWidth(60);
+    obj.layout15:setHeight(28);
+    obj.layout15:setName("layout15");
+
+    obj.edit15 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit15:setParent(obj.layout15);
+    obj.edit15:setTransparent(true);
+    obj.edit15:setFontSize(18.8);
+    obj.edit15:setFontColor("#8B0000");
+    lfm_setPropAsString(obj.edit15, "fontStyle",  "bold");
+    obj.edit15:setHorzTextAlign("center");
+    obj.edit15:setVertTextAlign("center");
+    obj.edit15:setLeft(0);
+    obj.edit15:setTop(0);
+    obj.edit15:setWidth(60);
+    obj.edit15:setHeight(29);
+    obj.edit15:setField("PHTotal");
+    obj.edit15:setName("edit15");
+
+    obj.layout16 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout16:setParent(obj.rectangle1);
+    obj.layout16:setLeft(503);
+    obj.layout16:setTop(1020);
+    obj.layout16:setWidth(165);
+    obj.layout16:setHeight(120.6);
+    obj.layout16:setName("layout16");
+
+    obj.textEditor1 = GUI.fromHandle(_obj_newObject("textEditor"));
+    obj.textEditor1:setParent(obj.layout16);
+    obj.textEditor1:setLeft(0);
+    obj.textEditor1:setTop(0);
+    obj.textEditor1:setWidth(165);
+    obj.textEditor1:setHeight(120.6);
+    obj.textEditor1:setFontSize(16.8);
+    obj.textEditor1:setFontColor("#8B0000");
+    obj.textEditor1:setField("CarInterpPositivas");
+    obj.textEditor1:setTransparent(true);
+    obj.textEditor1:setName("textEditor1");
+
+    obj.layout17 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout17:setParent(obj.rectangle1);
+    obj.layout17:setLeft(721);
+    obj.layout17:setTop(1020);
+    obj.layout17:setWidth(165);
+    obj.layout17:setHeight(120.6);
+    obj.layout17:setName("layout17");
+
+    obj.textEditor2 = GUI.fromHandle(_obj_newObject("textEditor"));
+    obj.textEditor2:setParent(obj.layout17);
+    obj.textEditor2:setLeft(0);
+    obj.textEditor2:setTop(0);
+    obj.textEditor2:setWidth(165);
+    obj.textEditor2:setHeight(120.6);
+    obj.textEditor2:setFontSize(16.8);
+    obj.textEditor2:setFontColor("#8B0000");
+    obj.textEditor2:setField("CarInterpNegativas");
+    obj.textEditor2:setTransparent(true);
+    obj.textEditor2:setName("textEditor2");
+
+    function obj:_releaseEvents()
+    end;
+
+    obj._oldLFMDestroy = obj.destroy;
+
+    function obj:destroy() 
+        self:_releaseEvents();
+
+        if (self.handle ~= 0) and (self.setNodeDatabase ~= nil) then
+          self:setNodeDatabase(nil);
+        end;
+
+        if self.layout12 ~= nil then self.layout12:destroy(); self.layout12 = nil; end;
+        if self.edit14 ~= nil then self.edit14:destroy(); self.edit14 = nil; end;
+        if self.edit4 ~= nil then self.edit4:destroy(); self.edit4 = nil; end;
+        if self.layout4 ~= nil then self.layout4:destroy(); self.layout4 = nil; end;
+        if self.layout11 ~= nil then self.layout11:destroy(); self.layout11 = nil; end;
+        if self.layout15 ~= nil then self.layout15:destroy(); self.layout15 = nil; end;
+        if self.edit15 ~= nil then self.edit15:destroy(); self.edit15 = nil; end;
+        if self.layout9 ~= nil then self.layout9:destroy(); self.layout9 = nil; end;
+        if self.layout10 ~= nil then self.layout10:destroy(); self.layout10 = nil; end;
+        if self.image1 ~= nil then self.image1:destroy(); self.image1 = nil; end;
+        if self.edit11 ~= nil then self.edit11:destroy(); self.edit11 = nil; end;
+        if self.textEditor1 ~= nil then self.textEditor1:destroy(); self.textEditor1 = nil; end;
+        if self.edit9 ~= nil then self.edit9:destroy(); self.edit9 = nil; end;
+        if self.edit5 ~= nil then self.edit5:destroy(); self.edit5 = nil; end;
+        if self.textEditor2 ~= nil then self.textEditor2:destroy(); self.textEditor2 = nil; end;
+        if self.edit6 ~= nil then self.edit6:destroy(); self.edit6 = nil; end;
+        if self.edit13 ~= nil then self.edit13:destroy(); self.edit13 = nil; end;
+        if self.layout5 ~= nil then self.layout5:destroy(); self.layout5 = nil; end;
+        if self.edit7 ~= nil then self.edit7:destroy(); self.edit7 = nil; end;
+        if self.layout17 ~= nil then self.layout17:destroy(); self.layout17 = nil; end;
+        if self.edit3 ~= nil then self.edit3:destroy(); self.edit3 = nil; end;
+        if self.edit12 ~= nil then self.edit12:destroy(); self.edit12 = nil; end;
+        if self.layout6 ~= nil then self.layout6:destroy(); self.layout6 = nil; end;
+        if self.edit8 ~= nil then self.edit8:destroy(); self.edit8 = nil; end;
+        if self.layout14 ~= nil then self.layout14:destroy(); self.layout14 = nil; end;
+        if self.layout16 ~= nil then self.layout16:destroy(); self.layout16 = nil; end;
+        if self.layout13 ~= nil then self.layout13:destroy(); self.layout13 = nil; end;
+        if self.layout3 ~= nil then self.layout3:destroy(); self.layout3 = nil; end;
+        if self.edit2 ~= nil then self.edit2:destroy(); self.edit2 = nil; end;
+        if self.edit10 ~= nil then self.edit10:destroy(); self.edit10 = nil; end;
+        if self.layout8 ~= nil then self.layout8:destroy(); self.layout8 = nil; end;
+        if self.layout1 ~= nil then self.layout1:destroy(); self.layout1 = nil; end;
+        if self.scrollBox1 ~= nil then self.scrollBox1:destroy(); self.scrollBox1 = nil; end;
+        if self.rectangle1 ~= nil then self.rectangle1:destroy(); self.rectangle1 = nil; end;
+        if self.edit1 ~= nil then self.edit1:destroy(); self.edit1 = nil; end;
+        if self.layout2 ~= nil then self.layout2:destroy(); self.layout2 = nil; end;
+        if self.layout7 ~= nil then self.layout7:destroy(); self.layout7 = nil; end;
+        self:_oldLFMDestroy();
+    end;
+
+    obj:endUpdate();
+
+    return obj;
+end;
+
+function newfrmFichaOdisseia2_01_svg()
+    local retObj = nil;
+    __o_rrpgObjs.beginObjectsLoading();
+
+    __o_Utils.tryFinally(
+      function()
+        retObj = constructNew_frmFichaOdisseia2_01_svg();
+      end,
+      function()
+        __o_rrpgObjs.endObjectsLoading();
+      end);
+
+    assert(retObj ~= nil);
+    return retObj;
+end;
+
+local _frmFichaOdisseia2_01_svg = {
+    newEditor = newfrmFichaOdisseia2_01_svg, 
+    new = newfrmFichaOdisseia2_01_svg, 
+    name = "frmFichaOdisseia2_01_svg", 
+    dataType = "", 
+    formType = "undefined", 
+    formComponentName = "form", 
+    title = "", 
+    description=""};
+
+frmFichaOdisseia2_01_svg = _frmFichaOdisseia2_01_svg;
+Firecast.registrarForm(_frmFichaOdisseia2_01_svg);
+
+return _frmFichaOdisseia2_01_svg;
